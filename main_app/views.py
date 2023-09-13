84a9244f5f2dc27d785fc1e333fa1a5c1e8f81ea
dani412 <dlnunez412@gmail.com>
2023-09-13 00:52:04 -0400

    adds About and All Finches

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+finches = [ 
+    {'name': 'Canary', 'species': 'Serinus canaria', 'description': 'streak-backed greenish brown', 'lifespan': 15},
+
+]
+
 # Create your views here.
+
+# define the home view
+def home(request):
+    # unlike EJS templating, we need the html file extension
+    return render(request, 'home.html')
+
+# define the about view
+def about(request):
+    # unlike EJS templating, we need the html file extension
+    return render(request, 'about.html')
+
+# define the All Finches view
+def finches_index(request):
+    return render(request, 'finches/index.html', { 'finches': finches })
